b6535969309b61bd50e5e7bcceeb480764495587
Sam Larsen <slarsen@meta.com>
2024-05-02 14:25:35 -0700

    [inductor] Add ProcessPool-in-subproc worker_start_method
    ghstack-source-id: 891763fdea530f94dd8ad6f74e751d0ca9793e44
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/122941

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -17,7 +17,6 @@ import platform
 import re
 import shlex
 import shutil
-import signal
 import struct
 import subprocess
 import sys
@@ -32,8 +31,7 @@ from copy import copy
 from ctypes import c_void_p, cdll, CDLL
 from functools import partial
 from pathlib import Path
-from threading import Thread
-from time import sleep, time
+from time import time
 from types import ModuleType
 from typing import (
     Any,
@@ -53,6 +51,12 @@ from torch._dynamo.device_interface import get_registered_device_interfaces
 from torch._dynamo.utils import counters, dynamo_timed
 from torch._inductor import config, exc, metrics
 from torch._inductor.codegen.cuda import cuda_env
+from torch._inductor.compile_worker.subproc_pool import (
+    _warm_process_pool,
+    AnyPool,
+    SubprocPool,
+)
+from torch._inductor.compile_worker.watchdog import _async_compile_initializer
 from torch._inductor.runtime.compile_tasks import (
     _module_to_triton_kernel,
     _reload_python_module,
@@ -2851,33 +2855,6 @@ class LambdaFuture(CodeCacheFuture):
         return self.result_fn()
 
 
-# If this process dies abnormally (e.g. segfault)
-# it will not shut down the workers. Instead
-# the workers will have their parent reassigned to the
-# init process. This launches a separate thread to
-# watch for the worker getting reassigned,
-# and cleans it up in this case.
-#
-# This function cannot be an inner function since otherwise mp_context="spawn" would
-# not work for ProcessPoolExecutor since inner functions cannot be pickled.
-def _async_compile_initializer(orig_ppid) -> None:
-    def run() -> None:
-        while True:
-            sleep(1)
-            if orig_ppid != os.getppid():
-                os.kill(os.getpid(), signal.SIGKILL)
-
-    global _watchdog_thread
-    _watchdog_thread = Thread(target=run, daemon=True)
-    _watchdog_thread.start()
-    # Ignore Ctrl-C (i.e. SIGINT) sent to pool workers to avoid meaningless log spam.
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
-
-
-_watchdog_thread: Optional[Thread] = None
-
-AnyPool = Union[ProcessPoolExecutor, ThreadPoolExecutor]
-
 # Used to keep track of all process pools invoked so far.
 _pool_set: Set[AnyPool] = set()
 
@@ -2916,7 +2893,10 @@ class AsyncCompile:
     def process_pool() -> AnyPool:
         assert config.compile_threads > 1
         pool: AnyPool
-        if config.worker_start_method == "thread":
+        if config.worker_start_method == "subprocess":
+            # Wrapper around ProcessPoolExecutor forks in a new process we control
+            pool = SubprocPool(config.compile_threads)
+        elif config.worker_start_method == "thread":
             pool = ThreadPoolExecutor(config.compile_threads)
         else:
             # ensure properties have been calculated before processes
@@ -2942,32 +2922,7 @@ class AsyncCompile:
         if config.compile_threads <= 1:
             return
         _compile_start()
-        pool = cls.process_pool()
-        assert isinstance(
-            pool, ProcessPoolExecutor
-        ), "warm_pool not yet implemented for ThreadPool"
-
-        # We have to fork processes for compiler workers, but the more memory and other resources that are loaded, the
-        # slower the os.fork time is, quite drastically. It also holds the GIL so we can't put it on another thread.
-
-        # Examples:
-        # A simple x + x + x script: 10ms seconds in the middle of the program, 2ms at startup
-        # tf_efficientnet_b0 benchmark: 50ms! in the middle of the program , 3ms at startup
-
-        # So we want to start the workers early when it is still cheap, and also to allow the workers to get
-        # ready before we have work for them.
-
-        # ProcessPoolExecutor also does not launch the workers until it finds a point when all the workers are idle.
-        # But if we waited until then fork time will be long and we will be waiting for the processes to initialize.
-
-        # We force them to start here with some YOLOing of the internal methods.
-        if hasattr(pool, "_start_queue_management_thread"):
-            pool._start_queue_management_thread()
-        else:
-            for _ in range(config.compile_threads):
-                pool._adjust_process_count()
-            if hasattr(pool, "_start_executor_manager_thread"):
-                pool._start_executor_manager_thread()
+        _warm_process_pool(cls.process_pool(), config.compile_threads)
         _compile_end()
 
     @classmethod
@@ -3057,6 +3012,8 @@ if (
     or os.environ.get("TORCH_WARM_POOL", "1") != "1"
     # TODO(jansel): do we need a version of warm_pool for threads?
     or config.worker_start_method == "thread"
+    # TODO(jansel): warm pool is broken, need to fix it
+    or config.worker_start_method == "subprocess"
 ):
     pass
 elif sys.version_info >= (3, 12):
--- torch/_inductor/config.py
+++ torch/_inductor/config.py
@@ -363,8 +363,8 @@ is_nightly_or_source = "dev" in torch.__version__ or "git" in torch.__version__
 developer_warnings = is_fbcode() or is_nightly_or_source
 
 # The multiprocessing start method to use for inductor workers in the codecache.
-# "thread", "fork", or "spawn"
-worker_start_method = "thread"
+# "subprocess", "thread", "fork", or "spawn"
+worker_start_method = "subprocess"
 
 # Flags to turn on all_reduce fusion. These 2 flags should be automaticaly turned
 # on by DDP and should not be set by the users.
