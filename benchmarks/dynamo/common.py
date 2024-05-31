59c7fbf3f7610d63e0b1644362e0128c13318852
Simon Fan <xmfan@meta.com>
2024-05-30 18:36:56 -0700

    enable CA, disable TLS

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -3394,6 +3394,7 @@ def parse_args(args=None):
         "--compiled-autograd",
         action="store_true",
         help="Enables compiled autograd on compiled benchmark",
+        default=True,
     )
 
     parser.add_argument(
