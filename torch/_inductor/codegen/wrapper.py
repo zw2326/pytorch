aa43af07c763c487ec5ac238fd1b16fefa9021a0
Bin Bao <binbao@meta.com>
2024-06-23 19:25:01 -0700

    [AOTI] Introduce DeferredCudaGridLine
    Summary: Similar to https://github.com/pytorch/pytorch/pull/129135, use DeferredCudaGridLine to create a deferred grid computation line when generating cpp wrapper.
    
    ghstack-source-id: 149552240cf3112e210c83739af3732684e62c76
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/129268

--- torch/_inductor/codegen/wrapper.py
+++ torch/_inductor/codegen/wrapper.py
@@ -1470,7 +1470,7 @@ class WrapperCodeGen(CodeGen):
             """
         )
 
-    def generate_default_grid(self, name: str, grid_args: List[Any]):
+    def generate_default_grid(self, kernel_name: str, grid_args: List[Any]):
         return grid_args
 
     def prepare_triton_kernel_call(self, device_index, call_args):
@@ -1553,6 +1553,7 @@ class WrapperCodeGen(CodeGen):
         raw_args=None,
         grid_fn: str = "grid",
         triton_meta=None,
+        autotune_configs=None,
     ):
         """
         Generates kernel call code.
