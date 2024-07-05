e8d5aa5a469f658d1897b373adf1faa4a9764b47
chilli <chilli@meta.com>
2024-07-05 16:18:00 -0700

    Added compile option to create_block_mask
    ghstack-source-id: c050e0018f3ebbb6f1f0f7fe11bcb94a99ce5164
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130106

--- torch/nn/attention/_flex_attention.py
+++ torch/nn/attention/_flex_attention.py
@@ -235,6 +235,7 @@ def _create_mask(
     M: int,
     N: int,
     device: str = "cuda",
+    _compiled: bool = False,
 ):
     r"""This function creates a mask tensor from a score_mod function.
 
@@ -249,18 +250,42 @@ def _create_mask(
     Returns:
         mask (Tensor): A mask tensor with shape (B, H, M, N).
     """
+    from contextlib import nullcontext
+
     b = torch.arange(0, B, device=device)
     h = torch.arange(0, H, device=device)
     m = torch.arange(0, M, device=device)
     n = torch.arange(0, N, device=device)
-    score_mod = torch.vmap(score_mod, in_dims=(0, None, None, None, 0))
-    score_mod = torch.vmap(score_mod, in_dims=(0, None, None, 0, None))
-    score_mod = torch.vmap(score_mod, in_dims=(0, None, 0, None, None))
-    score_mod = torch.vmap(score_mod, in_dims=(0, 0, None, None, None))
-    with TransformGetItemToIndex():
+    # TODO: fix this
+    # A hack required because of lack of torchfunctionmode support
+    # Working around some bugs with compiling vmap
+    if _compiled:
+        ctx = nullcontext()
+        b = b.view(B, 1, 1, 1)
+        h = h.view(1, H, 1, 1)
+        m = m.view(1, 1, M, 1)
+        n = n.view(1, 1, 1, N)
+    else:
+        ctx = TransformGetItemToIndex()  # type: ignore[assignment]
+        score_mod = torch.vmap(score_mod, in_dims=(0, None, None, None, 0))
+        score_mod = torch.vmap(score_mod, in_dims=(0, None, None, 0, None))
+        score_mod = torch.vmap(score_mod, in_dims=(0, None, 0, None, None))
+        score_mod = torch.vmap(score_mod, in_dims=(0, 0, None, None, None))
+
+    with ctx:
         out = score_mod(torch.zeros(B, H, M, N, device=device), b, h, m, n)
-    mask = torch.where(torch.isinf(out), False, True)
+        mask = torch.where(torch.isneginf(out), False, True)
     return mask
+
+
+# Done as a workaround around torch.compile not compiling what we want in the
+# presence of the torchfunctionmdoe
+def _create_block_mask_inner(
+    score_mod, B, H, M, N, device, KV_BLOCK_SIZE, Q_BLOCK_SIZE
+):
+    mask = _create_mask(score_mod, B, H, M, N, device, _compiled=True)
+    block_mask = _create_block_mask_from_mask(mask, KV_BLOCK_SIZE, Q_BLOCK_SIZE)
+    return block_mask
 
 
 def _create_block_mask(
@@ -272,6 +297,7 @@ def _create_block_mask(
     device: str = "cuda",
     KV_BLOCK_SIZE: int = _DEFAULT_SPARSE_BLOCK_SIZE,
     Q_BLOCK_SIZE: int = _DEFAULT_SPARSE_BLOCK_SIZE,
+    _compiled=True,
 ):
     r"""This function creates a block mask tuple from a score_mod function.
 
@@ -289,9 +315,12 @@ def _create_block_mask(
         block_mask (tuple): A tuple of (kv_num_blocks, kv_indices, q_num_blocks, q_indices,
                             KV_BLOCK_SIZE, Q_BLOCK_SIZE) which represents the block mask.
     """
-    mask = _create_mask(score_mod, B, H, M, N, device)
-    block_mask = _create_block_mask_from_mask(mask, KV_BLOCK_SIZE, Q_BLOCK_SIZE)
-    return block_mask
+    inner_func = _create_block_mask_inner
+    # This is kind of a temporary hack to workaround some issues
+    if _compiled:
+        inner_func = torch.compile(inner_func, fullgraph=True)
+    with TransformGetItemToIndex():
+        return inner_func(score_mod, B, H, M, N, device, KV_BLOCK_SIZE, Q_BLOCK_SIZE)
 
 
 """
