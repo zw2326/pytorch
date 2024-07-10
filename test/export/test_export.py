f20c0b9132ee7fb52d6ea3ffd3a546a0c5e2dc4f
Yidi Wu <yidi@meta.com>
2024-07-10 10:55:44 -0700

    Caching attr_proxy for nn_module attribute to fix guard check failure
    ghstack-source-id: 27a9dee9d1ebae8d13f24a8e9ca9ee98f76424fc
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130280

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -3601,8 +3601,6 @@ def forward(self, x):
         ):
             torch.export.export(exported_v2.module(), (torch.randn(2, 2),))
 
-    # https://github.com/pytorch/pytorch/issues/129939
-    @testing.expectedFailureNonStrict
     def test_export_cond(self):
         class A(torch.nn.Module):
             def __init__(self):
@@ -4992,8 +4990,6 @@ graph():
         )
 
     # Guard validation upsets the guard
-    # https://github.com/pytorch/pytorch/issues/129939
-    @unittest.expectedFailure
     def test_cond_with_module_stack_export_with(self):
         class Bar(torch.nn.Module):
             def __init__(self):
