3f00a5c8cabdb678bad8130d84488d29379fd0b3
James Perng <jamesperng@meta.com>
2024-07-11 16:21:03 -0700

    [pytorch][logging] move the dynamiccounter implementation into pytorch
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130579
    
    as titled;
    ghstack-source-id: 233464848
    @exported-using-ghexport
    
    Differential Revision: [D59405761](https://our.internmc.facebook.com/intern/diff/D59405761/)

--- torch/csrc/monitor/stats.cpp
+++ torch/csrc/monitor/stats.cpp
@@ -1,17 +1,35 @@
 #ifndef FBCODE_CAFFE2
 #include <torch/csrc/monitor/stats.h>
 #include <chrono>
+#include <string>
 
 namespace torch {
 namespace monitor {
+
+void registerCallback(
+    const std::string& key,
+    const std::function<double()>& callback) {
+  // implement
+}
+void unregisterCallback(const std::string& key) {
+  // implement
+}
 
 namespace detail {
 class StatImpl {};
 } // namespace detail
 
-IntegralStat::IntegralStat(std::string_view key) : impl_(nullptr) {}
+PeriodicAvgStat::PeriodicAvgStat(std::string_view key) : impl_(nullptr) {}
 
-void IntegralStat::addValue(
+void PeriodicAvgStat::addValue(
+    double value,
+    std::chrono::steady_clock::time_point now) {
+  // implement
+}
+
+PeriodicSumStat::PeriodicSumStat(std::string_view key) : impl_(nullptr) {}
+
+void PeriodicSumStat::addValue(
     double value,
     std::chrono::steady_clock::time_point now) {
   // implement
