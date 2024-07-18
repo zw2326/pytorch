53fe1640a56ec7c160f8b5ececa25298f5972c0c
Tom Ritchford <tom@swirly.com>
2024-07-18 13:15:09 +0000

    Add decomposition for unsqueeze_copy
    * Extracted from #128416
    
    ghstack-source-id: ce14646e22645c87379c717325055a59ff3b323e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130942

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -294,6 +294,7 @@ __all__ = [
     "unfold",
     "unfold_copy",
     "unsqueeze",
+    "unsqueeze_copy",
     "view",
     "view_as",
     "view_copy",
@@ -6324,6 +6325,7 @@ expand_copy = _make_copy_from_view(aten.expand)
 narrow_copy = _make_copy_from_view(aten.narrow)
 t_copy = _make_copy_from_view(aten.t)
 squeeze_copy = _make_copy_from_view(aten.squeeze)
+unsqueeze_copy = _make_copy_from_view(aten.unsqueeze)
 view_copy = _make_copy_from_view(aten.view)
 
 
