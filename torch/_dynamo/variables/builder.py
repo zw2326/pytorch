d52457b531b734c0939efd6bb4b406855f92d133
Yidi Wu <yidi@meta.com>
2024-07-24 09:48:47 -0700

    [dynamo] lazily guard and specialize on the symint when used in f-string
    ghstack-source-id: c72098f4bc3fa3a31506b20f9611404466505ead
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/131529

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -2633,6 +2633,8 @@ class SourcelessBuilder:
             return DeviceMeshVariable(value)
         elif isinstance(value, re.Pattern):
             return RegexPatternVariable(value)
+        elif isinstance(value, torch._dynamo.variables.lazy.LazySymNodeFormatString):
+            return ConstantVariable.create(str(value))
         unimplemented(
             f"Unexpected type in sourceless builder {value_type.__module__}.{value_type.__qualname__}"
         )
