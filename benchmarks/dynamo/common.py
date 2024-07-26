56a3e2c584292db2b737bc731b645c3089d42de8
Brian Hirsh <hirsheybar@fb.com>
2024-07-26 05:46:51 -0700

    track number of cpp->python exceptions thrown in torch.compile benchmark suite
    ghstack-source-id: d7deac4c579516945b809f1a81f70f7e136ffeaf
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/131481

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -557,6 +557,7 @@ class Stats:
         ok = torch._dynamo.utils.counters["frames"]["ok"]
         total = torch._dynamo.utils.counters["frames"]["total"]
         torch._dynamo.utils.counters.clear()
+        torch._C._clear_cpp_to_python_translated_exception_count(),
         return ok, total
 
     @classmethod
@@ -2211,6 +2212,7 @@ def get_dynamo_stats():
             "cudagraph_skips": torch._dynamo.utils.counters["inductor"][
                 "cudagraph_skips"
             ],
+            "cpp_to_python_exceptions_raised": torch._C._get_cpp_to_python_translated_exception_count(),
         }
     )
 
