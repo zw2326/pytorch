5a5835dee178c7bc0094252dc5e7b0773e65f2cf
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-07-29 12:26:54 +0800

    Move class `torch.nn.Parameter` out of module `torch.nn`
    ghstack-source-id: d067bbc45c827e6e7dfd445eb13a16c5318cb87f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130327

--- torch/nn/parameter.pyi
+++ torch/nn/parameter.pyi
@@ -1,47 +1,11 @@
-from typing_extensions import TypeGuard
+from torch._parameter import (
+    is_lazy as is_lazy,
+    Parameter as _Parameter,
+    UninitializedBuffer as _UninitializedBuffer,
+    UninitializedParameter as _UninitializedParameter,
+)
 
-from torch import dtype, Tensor
-from torch._C import _TensorMeta
-from torch.types import Device
-
-class _ParameterMeta(_TensorMeta):
-    def __instancecheck__(self, instance: object) -> bool: ...
-
-class Parameter(Tensor, metaclass=_ParameterMeta):
-    def __init__(
-        self,
-        data: Tensor | None = None,
-        requires_grad: bool = True,
-    ) -> None: ...
-
-def is_lazy(
-    param: Tensor,
-) -> TypeGuard[UninitializedParameter | UninitializedBuffer]: ...
-
-class UninitializedParameter(Parameter):
-    def __init__(
-        self,
-        requires_grad: bool = True,
-        device: Device = None,
-        dtype: dtype | None = None,
-    ) -> None: ...
-    def materialize(
-        self,
-        shape: tuple[int, ...],
-        device: Device = None,
-        dtype: dtype | None = None,
-    ) -> None: ...
-
-class UninitializedBuffer(Tensor):
-    def __init__(
-        self,
-        requires_grad: bool = False,
-        device: Device = None,
-        dtype: dtype | None = None,
-    ) -> None: ...
-    def materialize(
-        self,
-        shape: tuple[int, ...],
-        device: Device = None,
-        dtype: dtype | None = None,
-    ) -> None: ...
+# workaround for setting the __module__ attribute in stub file
+class Parameter(_Parameter): ...
+class UninitializedParameter(_UninitializedParameter): ...
+class UninitializedBuffer(_UninitializedBuffer): ...
